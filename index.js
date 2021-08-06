7b8ccaa305cd52cff83534b978d4a2772e0b6687
Stephen Freeman <stephen.freeman.dev@gmail.com>
2021-08-05 17:41:59 -0700

    added cors function

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ require("dotenv").config();
 app.use(cors());
 app.use(helmet());
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const port = process.env.PORT || 5001;
 mongoose.Promise = global.Promise;
