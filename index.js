30dfed1b05dcdbc5cffb66c3a572a21c03b8d62b
Stephen Freeman <stephen.freeman.dev@gmail.com>
2021-08-05 17:55:44 -0700

    changed index.js to be server also

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const app = require("./server");
-const mongoose = require("mongoose");
-const helmet = require('helmet');
-const cors = require('cors');
-require("dotenv").config();
-
-app.use(cors());
-app.use(helmet());
-
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+// const cors = require('cors');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+require('./routes/sneaks.routes.js')(app);
+require('dotenv').config();
 
-const port = process.env.PORT || 5001;
+
+var port = process.env.PORT || 5000;
 mongoose.Promise = global.Promise;
 
-app.listen(port, () => {
-  console.log("Sneaks app listening on port ", port);
-});
+app.listen(port, function () {
+  console.log(`Sneaks app listening on port `, port);
+ });
 
+module.exports = app;
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require("express");
-const app = express();
-const sneakerRoutes = require("./routes/sneaks.routes");
+// const express = require("express");
+// const app = express();
+// const sneakerRoutes = require("./routes/sneaks.routes");
+// const helmet = require('helmet');
+// const cors = require('cors');
+// app.use(cors());
+// app.use(helmet());
 
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
-app.use("api/sneakers", sneakerRoutes);
+// app.use("api/sneakers", sneakerRoutes);
 
-app.get("/", function(req, res) {
-  res.json({msg: "Welcome to Gail Hosue api"});
-});
+// app.get("/", function(req, res) {
+//   res.json({msg: "Welcome to Gail Hosue api"});
+// });
 
-module.exports = app;
+// module.exports = app;
 
