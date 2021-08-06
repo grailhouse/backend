baeece034a056e9de36a4dc5b2313d398c3dfe50
Stephen Freeman <stephen.freeman.dev@gmail.com>
2021-08-05 18:39:15 -0700

    added cors function

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const cors = require('cors');
 server.use(cors());
 server.use(helmet());
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 server.use("/api", sneakerRoutes);
 
