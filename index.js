1298d265cd0109e52994d8d2f97ccdb87375affb
Stephen Freeman <stephen.freeman.dev@gmail.com>
2021-08-05 17:30:52 -0700

    added cors and helmet

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = require("./server");
 const mongoose = require("mongoose");
+const helmet = require('helmet');
+const cors = require('cors');
 require("dotenv").config();
+
+app.use(cors());
+app.use(helmet());
+
 
 
 const port = process.env.PORT || 5001;
